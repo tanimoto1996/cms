32d727f170ebfd17ff6c5f329ff63a603f20a125
tanimoto <yumenikki2@gmail.com>
2021-09-03 20:27:48 +0900

    モデル処理作成。コントローラーの一覧・詳細（route未設定）の処理追加。一覧・詳細画面追加。

--- laravel/routes/front.php
+++ laravel/routes/front.php
@@ -3,8 +3,7 @@
 // ファサードとはインスタンス化しなくても使えるクラス
 // サービスコンテナ（AppServiceProvider.php）に登録することで使える
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
-
-Route::get('/', function () {
-  echo 'front';
-});
+Route::get('/', [PostController::class, 'index'])->name('home');
+Route::resource('posts', PostController::class)->only(['index', 'show']);
